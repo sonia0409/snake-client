425c75dff6841fb53cf64e89591771f663172456
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-01 18:36:08 +0000

    updated or called a new event'data'

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost', // IP address here,
+    host: '165.227.47.243', // IP address here,
     port: 50541// PORT number here,
   });
 
@@ -12,6 +12,11 @@ const connect = function () {
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log(`I am connected`)
+    //conn.write("Move: up")
+  });
+  conn.on("data", (data) => {
+    // code that does something when the connection is first established
+    console.log(`from server: ${data}`)
     //conn.write("Move: up")
   });
   return conn;
