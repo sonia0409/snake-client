d38d9011bd99f8accad1bea2c9a2c1eb19b71b08
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-01 19:11:14 +0000

    replied to the server

--- client.js
+++ client.js
@@ -8,15 +8,15 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log(`Successfully connected to game server`)
-    //conn.write("Move: up")
+    conn.write("Name: SS")
   });
   conn.on("data", (data) => {
-    // code that does something when the connection is first established
+    // code prints out the data, obtined from server when the connection is first established
     console.log(`from server: ${data}`)
-    //conn.write("Move: up")
   });
   return conn;
 };
